e15c56a687406b421212d08f04a5ba2b4cf4cd8c
HyodaKazuaki <kazuaki@ex-t.jp>
2021-03-15 16:02:12 +0900

    Add initial project

---
+++ src/main/java/com/example/astah/Activator.kt
@@ -0,0 +1,9 @@
+package com.example.astah
+
+import org.osgi.framework.BundleActivator
+import org.osgi.framework.BundleContext
+
+class Activator : BundleActivator {
+    override fun start(context: BundleContext?) {}
+    override fun stop(context: BundleContext?) {}
+}
